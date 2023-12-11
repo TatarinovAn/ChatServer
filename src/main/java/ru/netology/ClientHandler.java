9e00dbcd4f2c8726790712f7989c03daf7cb3adb
Tatarinov A <vekwork@bk.ru>
2023-12-11 21:39:34 +0300

    change ClientHandler and Server

--- src/main/java/ru/netology/ClientHandler.java
+++ src/main/java/ru/netology/ClientHandler.java
@@ -1,5 +1,8 @@
 package ru.netology;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
@@ -15,15 +18,61 @@ public class ClientHandler implements Runnable {
         try {
             this.server = server;
             this.clientSocket = socket;
+            System.out.println(socket);
             this.inMassage = new Scanner(socket.getInputStream());
             this.outMessage = new PrintWriter(socket.getOutputStream());
-        } catch (Exception e) {
-
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
     }
 
+
     @Override
     public void run() {
+        try {
+            // сервер отправляет сообщение
+            server.sendMessageToAllClient("Новый участник вошёл в чат!");
 
+
+            while (true) {
+                if (inMassage.hasNext()) {
+
+                    String clientMessage = inMassage.nextLine();
+                    if (clientMessage.contains("/exit")) {
+                        break;
+                    }
+                    File fileChat = new File("D://Chat/Mes_Server"); // Создание файла сообщений
+                    fileChat.mkdirs();
+                    try (FileWriter writer = new FileWriter("D://Chat/Mes_Server/mes.txt", true)) {
+                        writer.write(clientMessage);
+                        writer.append('\n');
+                        writer.flush();
+                    } catch (IOException ex) {
+                        System.out.println(ex.getMessage());
+                    }
+                    server.sendMessageToAllClient(clientMessage);
+                }
+                Thread.sleep(100);
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            this.close();
+        }
+    }
+
+    public void sendMsg(String msg) {
+        try {
+            outMessage.println(msg);
+            outMessage.flush();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void close() {
+        // удаляем клиента из списка
+        server.removeClient(this);
+        sendMsg("Покинул чат");
     }
 }
--- src/main/java/ru/netology/Server.java
+++ src/main/java/ru/netology/Server.java
@@ -8,27 +8,43 @@ import java.util.ArrayList;
 public class Server {
     private static final int PORT = 8080;
     private ArrayList<ClientHandler> clients = new ArrayList<>();
-    public static void main(String[] args) throws IOException {
-        System.out.println("Start chat");
-            File file = new File("D://Chat/Setting");
-            file.mkdirs();
-            try (FileWriter writer = new FileWriter("D://Chat/Setting/setting.txt", false)) {
-                writer.write(Integer.toString(PORT));
-                writer.flush();
-            } catch (IOException ex) {
-                System.out.println(ex.getMessage());
-            }
 
-        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+    public Server() {
+        Socket clientSocket = null;
+        File file = new File("D://Chat/Setting"); // Создание файла настроек
+        file.mkdirs();
+        try (FileWriter writer = new FileWriter("D://Chat/Setting/setting.txt", false)) {
+            writer.write(Integer.toString(PORT));
+            writer.flush();
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) // подключение сервера
+        {
+            System.out.println("Server Start");
+
             while (true) {
-                try (Socket clientSocket = serverSocket.accept();
-                     PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-                     BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                ) {
-
-
-                }
+                clientSocket = serverSocket.accept();
+                ClientHandler client = new ClientHandler(clientSocket, this);
+                clients.add(client);
+                System.out.println("Колличество клиентов: " + clients.size());
+                new Thread(client).start();
             }
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
     }
+
+    public void sendMessageToAllClient(String message) {
+        for (ClientHandler entry : clients) {
+            entry.sendMsg(message);
+        }
+    }
+
+    public void removeClient(ClientHandler client) {
+        clients.remove(client);
+    }
 }
+
